86b46946d63fb26bcb18c688f99ede52344427dd
Richiul <rpalkuc@yahoo.com>
2022-11-25 15:58:24 +0200

    created anime tracker app vue

--- geocoding-map/server/index.js
+++ geocoding-map/server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
-require('dotenv').config();
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
@@ -12,17 +12,17 @@ const app = express();
 app.use(cors());
 
 //routes
-app.get('/api/search/:query', async (req, res) => {
-    try {
-        const query = req.params.query;
-        const result = await axios(`https://api.mapbox.com/search/v1/forward/starbucks?language=en&country=US&access_token=pk.eyJ1Ijoia2ludnNyaW5nMTIzIiwiYSI6ImNsYXU1OHJ3bDAwamQzcG1raXE4cHF5Z3AifQ.PsjS66KSkl4MOBVV7-_igQ`
-        );
-        console.log(result.data);
-    }
-    catch (err) {
-        res.status(500).json({error: err.message});
-    } 
-    
+app.get("/api/search/:query", async (req, res) => {
+  try {
+    const query = req.params.query;
+    const result = await axios(
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?access_token=${process.env.VUE_APP_API_KEY}`
+    );
+    const data = result.data;
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 app.listen(PORT, () => console.log(`app has started on port: ${PORT}`));
